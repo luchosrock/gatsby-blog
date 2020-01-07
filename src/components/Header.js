92415e9fec1ab6c978e50be60034fd41306f33ef
Luis Bustamante <luchosrock@gmail.com>
2020-01-06 22:13:57 -0300

    taking a dump ahahah, day 3 of 100

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-import { Link } from "gatsby";
 import React from "react";
 export const Header = ({ siteTitle, siteDescription }) => (
   <div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import { graphql } from "gatsby";
 import React from "react";
 import { Layout } from "../components/Layout";
+import Dump from "../components/Dump";
 export default ({ data }) => {
   return (
     <>
+      <Dump data={data} />
       <Layout>
-        {data.allMdx.nodes.map(({ excerpt, frontmatter }) => (
+        {data.allMdx.nodes.map(({ frontmatter }) => (
           <>
             <h1>{frontmatter.title}</h1>
             <p>{frontmatter.date}</p>
