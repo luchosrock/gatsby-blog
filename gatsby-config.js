14d3a1be8c79e5d54f6923c966fcea344aa6a89d
Luis Bustamante <luchosrock@gmail.com>
2020-01-28 23:16:48 -0300

    handle cover images, day 8 of 100

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,18 +5,33 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
-        extensions: [`.mdx`, `.md`]
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590
+            }
+          }
+        ],
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590
+            }
+          }
+        ]
       }
     },
     {
       resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/posts`,
-        name: `posts`
-      }
+      options: { path: `${__dirname}/posts`, name: `posts` }
     }
   ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,38 @@
 import { graphql, Link } from "gatsby";
+import Img from "gatsby-image";
 import React from "react";
+import styled from "styled-components";
 import { Layout } from "../components/Layout";
-import Dump from "../components/Dump";
+
+const IndexWrapper = styled.main``;
+
+const PostWrapper = styled.div``;
+
+const Image = styled(Img)`
+  border-radius: 5px;
+`;
+
 export default ({ data }) => {
   return (
-    <>
-      <Layout>
+    <Layout>
+      <IndexWrapper>
         {data.allMdx.nodes.map(({ id, excerpt, frontmatter, fields }) => (
-          <Link to={fields.slug}>
-            <h1>{frontmatter.title}</h1>
-            <p>{frontmatter.date}</p>
-          </Link>
+          <PostWrapper key={id}>
+            <Link to={fields.slug}>
+              {!!frontmatter.cover ? (
+                <Image sizes={frontmatter.cover.childImageSharp.sizes} />
+              ) : null}
+              <h1>{frontmatter.title}</h1>
+              <p>{frontmatter.date}</p>
+              <p>{excerpt}</p>
+            </Link>
+          </PostWrapper>
         ))}
-      </Layout>
-    </>
+      </IndexWrapper>
+    </Layout>
   );
 };
+
 export const query = graphql`
   query SITE_INDEX_QUERY {
     allMdx(
@@ -27,7 +44,15 @@ export const query = graphql`
         excerpt(pruneLength: 250)
         frontmatter {
           title
-          date
+          date(formatString: "YYYY MMMM Do")
+          cover {
+            publicURL
+            childImageSharp {
+              sizes(maxWidth: 2000, traceSVG: { color: "#639" }) {
+                ...GatsbyImageSharpSizes_tracedSVG
+              }
+            }
+          }
         }
         fields {
           slug
